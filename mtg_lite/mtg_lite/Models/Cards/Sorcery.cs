fc48ec18f0ac42dc44abc43b93ee4d911809c360
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-11-09 10:45:38 -0500

    Update Sorcery.cs

--- mtg_lite/mtg_lite/Models/Cards/Sorcery.cs
+++ mtg_lite/mtg_lite/Models/Cards/Sorcery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using MTGO_lite.Models.Manas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,34 @@ namespace mtg_lite.Models.Cards
 {
     internal class Sorcery
     {
+
+        public Sorcery(string name, Mana manaCost, Bitmap picture) : base(name, manaCost, picture)
+        {
+        }
+
+        public static Sorcery Fabrique(string stringEntree)
+        {
+            try
+            {
+                switch (stringEntree)
+                {
+                    case "death_by_dragons":
+                        return new Sorcery(stringEntree, new Mana(0, 0, 0, 2, 0, 4), new Bitmap("death_by_dragons.pgn"));
+                        break;
+                    case "chain_lightning":
+                        return new Sorcery(stringEntree, new Mana(0, 0, 0, 0, 0, 1), new Bitmap("chain_lightning.pgn"));
+                        break;
+                    case "blightning":
+                        return new Sorcery(stringEntree, new Mana(1, 0, 0, 1, 0, 1), new Bitmap("blightning.pgn"));
+                        break;
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
