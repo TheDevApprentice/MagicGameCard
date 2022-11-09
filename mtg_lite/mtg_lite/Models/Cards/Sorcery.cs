1ec1e947d4703dc38dfa645fdead5004df0ebf04
TheDevApprentice <zoucodeparplaisir@outlook.com>
2022-11-09 10:58:35 -0500

    .

--- mtg_lite/mtg_lite/Models/Cards/Sorcery.cs
+++ mtg_lite/mtg_lite/Models/Cards/Sorcery.cs
@@ -1,6 +1,7 @@
 using MTGO_lite.Models.Manas;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,19 @@ namespace mtg_lite.Models.Cards
                 switch (stringEntree)
                 {
                     case "death_by_dragons":
-                        return new Sorcery(stringEntree, new Mana(0, 0, 0, 2, 0, 4), new Bitmap("death_by_dragons.pgn"));
-                        break;
+                        return new Sorcery(stringEntree, new Mana(0, 0, 0, 2, 0, 4), new Bitmap("death_by_dragons.png"));
+                       
                     case "chain_lightning":
-                        return new Sorcery(stringEntree, new Mana(0, 0, 0, 0, 0, 1), new Bitmap("chain_lightning.pgn"));
-                        break;
+                        return new Sorcery(stringEntree, new Mana(0, 0, 0, 0, 0, 1), new Bitmap("chain_lightning.png"));
+                       
                     case "blightning":
-                        return new Sorcery(stringEntree, new Mana(1, 0, 0, 1, 0, 1), new Bitmap("blightning.pgn"));
-                        break;
-
+                        return new Sorcery(stringEntree, new Mana(1, 0, 0, 1, 0, 1), new Bitmap("blightning.png"));
+                        
+                    default:
+                        return new Sorcery(stringEntree, new Mana(1, 0, 0, 1, 0, 1), new Bitmap("blightning.png")); ; 
+                        
                 }
+                
             }
             catch (Exception)
             {
