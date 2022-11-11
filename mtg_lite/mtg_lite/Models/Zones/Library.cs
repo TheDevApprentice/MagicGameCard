a84fd2b88f7403d85e275060ad3626ae30c6800e
PrivateCoolean <101284670+PrivateCoolean@users.noreply.github.com>
2022-11-11 14:11:33 -0500

    CREATION ZONEeNFANT

--- mtg_lite/mtg_lite/Models/Zones/Library.cs
+++ mtg_lite/mtg_lite/Models/Zones/Library.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using mtg_lite.Models.Cards;
+using mtg_lite.Models.Players;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace mtg_lite.Models.Zones
 {
-    internal class Library
+    public class Library : Zone
     {
+        public Library(List<Card> cards, Player player) : base(cards, player)
+        {
+        }
     }
 }
